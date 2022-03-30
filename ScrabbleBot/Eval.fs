664cfafabbac9e7884d28e65db4b688cf081a904
devilpant <peterbruhn_nielsen@yahoo.com>
2022-03-30 11:38:13 +0200

    Main

---
+++ ScrabbleBot/Eval.fs
@@ -0,0 +1,212 @@
+﻿// Insert your updated Eval.fs file here from Assignment 7. All modules must be internal.
+
+module internal Eval
+
+open StateMonad
+
+let hello =
+    [ ('H', 4)
+      ('E', 1)
+      ('L', 1)
+      ('L', 1)
+      ('O', 1) ]
+
+let state = mkState [ ("x", 5); ("y", 42) ] hello [ "_pos_"; "_result_" ]
+let emptyState = mkState [] [] []
+
+let isVowel c =
+    let upperC = System.Char.ToUpper(c)
+
+    (upperC = 'A'
+     || upperC = 'E'
+     || upperC = 'O'
+     || upperC = 'I'
+     || upperC = 'U')
+
+
+let add a b =
+    a >>= fun x -> b >>= fun y -> ret (x + y)
+
+let sub a b =
+    a >>= fun x -> b >>= fun y -> ret (x - y)
+
+let mul a b =
+    a >>= fun x -> b >>= fun y -> ret (x * y)
+
+let modu a b =
+    a
+    >>= fun x ->
+            b
+            >>= fun y ->
+                    if y <> 0 then
+                        ret (x % y)
+                    else
+                        fail DivisionByZero
+
+let div a b =
+    a
+    >>= fun x ->
+            b
+            >>= fun y ->
+                    if y <> 0 then
+                        ret (x / y)
+                    else
+                        fail DivisionByZero
+
+type aExp =
+    | N of int
+    | V of string
+    | WL
+    | PV of aExp
+    | Add of aExp * aExp
+    | Sub of aExp * aExp
+    | Mul of aExp * aExp
+    | Div of aExp * aExp
+    | Mod of aExp * aExp
+    | CharToInt of cExp
+
+and cExp =
+    | C of char (* Character value *)
+    | CV of aExp (* Character lookup at word index *)
+    | ToUpper of cExp
+    | ToLower of cExp
+    | IntToChar of aExp
+
+type bExp =
+    | TT (* true *)
+    | FF (* false *)
+
+    | AEq of aExp * aExp (* numeric equality *)
+    | ALt of aExp * aExp (* numeric less than *)
+
+    | Not of bExp (* boolean not *)
+    | Conj of bExp * bExp (* boolean conjunction *)
+
+    | IsVowel of cExp (* check for vowel *)
+    | IsLetter of cExp (* check for letter *)
+    | IsDigit of cExp (* check for digit *)
+
+let (.+.) a b = Add(a, b)
+let (.-.) a b = Sub(a, b)
+let (.*.) a b = Mul(a, b)
+let (./.) a b = Div(a, b)
+let (.%.) a b = Mod(a, b)
+
+let (~~) b = Not b
+let (.&&.) b1 b2 = Conj(b1, b2)
+
+let (.||.) b1 b2 =
+    ~~(~~b1 .&&. ~~b2) (* boolean disjunction *)
+
+let (.->.) b1 b2 =
+    (~~b1) .||. b2 (* boolean implication *)
+
+let (.=.) a b = AEq(a, b)
+let (.<.) a b = ALt(a, b)
+let (.<>.) a b = ~~(a .=. b)
+let (.<=.) a b = a .<. b .||. ~~(a .<>. b)
+
+let (.>=.) a b =
+    ~~(a .<. b) (* numeric greater than or equal to *)
+
+let (.>.) a b =
+    ~~(a .=. b)
+    .&&. (a .>=. b) (* numeric greater than *)
+
+let rec arithEval a : SM<int> =
+    match a with
+    | N n -> ret n
+    | V v -> lookup v
+    | WL -> wordLength
+    | PV x -> arithEval x >>= pointValue
+    | Add (x, y) -> add (arithEval x) (arithEval y)
+    | Sub (x, y) -> sub (arithEval x) (arithEval y)
+    | Mul (x, y) -> mul (arithEval x) (arithEval y)
+    | Div (x, y) -> div (arithEval x) (arithEval y)
+    | Mod (x, y) -> modu (arithEval x) (arithEval y)
+    | CharToInt c -> charEval c >>= (fun c -> ret (int c))
+
+and charEval c : SM<char> =
+    match c with
+    | C char -> ret char
+    | ToUpper x ->
+        charEval x
+        >>= (fun c -> ret (System.Char.ToUpper c))
+    | ToLower x ->
+        charEval x
+        >>= (fun c -> ret (System.Char.ToLower c))
+    | CV cv -> arithEval cv >>= characterValue
+    | IntToChar n -> arithEval n >>= (fun n -> ret (char n))
+
+let rec boolEval b : SM<bool> =
+    match b with
+    | TT -> ret true
+    | FF -> ret false
+    | AEq (a, b) ->
+        (arithEval a)
+        >>= fun x -> (arithEval b) >>= fun y -> ret (x = y)
+    | ALt (a, b) ->
+        (arithEval a)
+        >>= fun x -> (arithEval b) >>= fun y -> ret (x < y)
+    | Not x -> boolEval x >>= (fun x -> ret (not x))
+    | Conj (a, b) ->
+        (boolEval a)
+        >>= fun x -> (boolEval b) >>= fun y -> ret (x && y)
+    | IsDigit x ->
+        charEval x
+        >>= (fun c -> ret (System.Char.IsDigit c))
+    | IsLetter x ->
+        charEval x
+        >>= (fun c -> ret (System.Char.IsLetter c))
+    | IsVowel x -> charEval x >>= (fun c -> ret (isVowel c))
+
+
+
+type stm = (* statements *)
+    | Declare of string (* variable declaration *)
+    | Ass of string * aExp (* variable assignment *)
+    | Skip (* nop *)
+    | Seq of stm * stm (* sequential composition *)
+    | ITE of bExp * stm * stm (* if-then-else statement *)
+    | While of bExp * stm (* while statement *)
+
+let rec stmntEval stmnt : SM<unit> = failwith "Not implemented"
+
+(* Part 3 (Optional) *)
+
+type StateBuilder() =
+
+    member this.Bind(f, x) = f >>= x
+    member this.Return(x) = ret x
+    member this.ReturnFrom(x) = x
+    member this.Delay(f) = f ()
+    member this.Combine(a, b) = a >>= (fun _ -> b)
+
+let prog = new StateBuilder()
+
+let arithEval2 a = failwith "Not implemented"
+let charEval2 c = failwith "Not implemented"
+let rec boolEval2 b = failwith "Not implemented"
+
+let stmntEval2 stm = failwith "Not implemented"
+
+(* Part 4 *)
+
+type word = (char * int) list
+type squareFun = word -> int -> int -> Result<int, Error>
+
+let stmntToSquareFun stm = failwith "Not implemented"
+
+
+type coord = int * int
+
+type boardFun = coord -> Result<squareFun option, Error>
+
+let stmntToBoardFun stm m = failwith "Not implemented"
+
+type board =
+    { center: coord
+      defaultSquare: squareFun
+      squares: boardFun }
+
+let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
---
+++ ScrabbleBot/Parser.fs
@@ -0,0 +1,175 @@
+﻿// ScrabbleUtil contains the types coord, boardProg, and SquareProg. Remove these from your file before proceeding.
+// Also note that the modulse Ass7 and ImpParser have been merged to one module called Parser.
+
+// Insert your Parser.fs file here from Assignment 7. All modules must be internal.
+
+module internal Parser
+
+open ScrabbleUtil // NEW. KEEP THIS LINE.
+open System
+open Eval
+open FParsecLight.TextParser // Industrial parser-combinator library. Use for Scrabble Project.
+
+let pIntToChar = pstring "intToChar"
+let pPointValue = pstring "pointValue"
+
+let pCharToInt = pstring "charToInt"
+let pToUpper = pstring "toUpper"
+let pToLower = pstring "toLower"
+let pCharValue = pstring "charValue"
+
+let pTrue = pstring "true"
+let pFalse = pstring "false"
+let pIsDigit = pstring "isDigit"
+let pIsLetter = pstring "isLetter"
+let pIsVowel = pstring "isVowel"
+
+let pif = pstring "if"
+let pthen = pstring "then"
+let pelse = pstring "else"
+let pwhile = pstring "while"
+let pdo = pstring "do"
+let pdeclare = pstring "declare"
+
+let whitespaceChar = satisfy System.Char.IsWhiteSpace <?> "whitespace"
+let pletter = satisfy System.Char.IsLetter <?> "letter"
+
+let palphanumeric =
+    satisfy System.Char.IsLetterOrDigit
+    <?> "alphanumeric"
+
+let spaces = many whitespaceChar <?> "spaces"
+let spaces1 = many1 whitespaceChar <?> "space1"
+
+let (.>*>.) p1 p2 = p1 .>> spaces .>>. p2
+let (.>*>) p1 p2 = p1 .>> spaces .>> p2
+let (>*>.) p1 p2 = p1 .>> spaces >>. p2
+
+let parenthesise p =
+    between (pchar '(') (pchar ')') (spaces >>. p .>> spaces)
+
+let angleParenthesise p =
+    between (pchar '{') (pchar '}') (spaces >>. p .>> spaces)
+
+let pid =
+    let aux (list: char list) = System.String.Concat(list)
+
+    pletter <|> pchar ('_')
+    .>>. many (palphanumeric <|> pchar ('_'))
+    |>> fun (c, cl) -> aux (c :: cl)
+
+
+let unop op = (>*>.) op
+let binop op p1 p2 = p1 .>*>. unop op p2
+
+let TermParse, tref = createParserForwardedToRef<aExp> ()
+let ProdParse, pref = createParserForwardedToRef<aExp> ()
+let AtomParse, aref = createParserForwardedToRef<aExp> ()
+let CharParse, cref = createParserForwardedToRef<cExp> ()
+
+let AexpParse = TermParse
+let CexpParse = CharParse
+let BexpParse = pstring "not implemented"
+let stmParse = pstring "not implemented"
+
+let AddParse =
+    binop (pchar '+') ProdParse TermParse |>> Add
+    <?> "Add"
+
+let SubParse =
+    binop (pchar '-') ProdParse TermParse |>> Sub
+    <?> "Sub"
+
+do tref := choice [ AddParse; SubParse; ProdParse ]
+
+let MulParse =
+    binop (pchar '*') AtomParse ProdParse |>> Mul
+    <?> "Mul"
+
+let DivParse =
+    binop (pchar '/') AtomParse ProdParse |>> Div
+    <?> "Div"
+
+let ModParse =
+    binop (pchar '%') AtomParse ProdParse |>> Mod
+    <?> "Mod"
+
+do
+    pref
+    := choice [ MulParse
+                DivParse
+                ModParse
+                AtomParse ]
+
+let NegParse =
+    unop (pchar '-') pint32
+    |>> (fun n -> Mul(N -1, N n))
+    <?> "Neg"
+
+let NParse = pint32 |>> N <?> "Int"
+let PVParse = unop pPointValue AtomParse |>> PV <?> "PV"
+let VParse = pid |>> V <?> "Str"
+let ParParse = parenthesise TermParse
+
+let CharToIntParse =
+    unop pCharToInt (parenthesise CexpParse)
+    |>> CharToInt
+    <?> "CharToInt"
+
+let CParse =
+    between (pchar ''') (pchar ''') (anyChar) |>> C
+    <?> "Char"
+
+let CVParse =
+    unop pCharValue (parenthesise AexpParse) |>> CV
+    <?> "CV"
+
+let ToUpperParse =
+    unop pToUpper (parenthesise CexpParse) |>> ToUpper
+    <?> "ToUpper"
+
+let ToLowerParse =
+    unop pToLower (parenthesise CexpParse) |>> ToLower
+    <?> "ToLower"
+
+let IntToCharParse =
+    unop pIntToChar (parenthesise AexpParse)
+    |>> IntToChar
+    <?> "IntToChar"
+
+do
+    aref
+    := choice [ CharToIntParse
+                NegParse
+                NParse
+                PVParse
+                VParse
+                ParParse ]
+
+do
+    cref
+    := choice [ CVParse
+                ToUpperParse
+                ToLowerParse
+                IntToCharParse
+                CParse ]
+
+
+
+(* The rest of your parser goes here *)
+
+type word = (char * int) list
+type square = Map<int, word -> int -> int -> int>
+
+let parseSquareFun _ = failwith "not implemented"
+
+let parseBoardFun _ = failwith "not implemented"
+
+type boardFun = coord -> square option
+
+type board =
+    { center: coord
+      defaultSquare: square
+      squares: boardFun }
+
+let parseBoardProg (bp: boardProg) : board = failwith "not implemented"
