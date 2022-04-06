08882f6690fc2f689a7468bd729428469fda55be
Christian Emil Stender <ches@itu.dk>
2022-04-06 10:04:57 +0200

    Added Multiset fs fsi and dict fs fsi

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -1,9 +1,16 @@
-﻿// Insert your MultiSet.fs file here. All modules must be internal
+﻿module internal MultiSet
 
-module internal MultiSet
-
-    type MultiSet<'a> = Temp of unit // Not implemented
-
-    let empty : MultiSet<'a> = Temp () // Not implemented
-    let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented" 
-    let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
+    type MultiSet<'a when 'a : comparison> = 
+        | M of Map<'a, uint32>
+    
+    let empty = M Map.empty
+    let isEmpty (M s) = s.IsEmpty
+    let size (M s) = if isEmpty (M s) then 0u else s |> Seq.sumBy(fun i -> i.Value)
+    let contains a (M s) = s.ContainsKey(a)
+    let numItems a (M s) = if contains a (M s) then s.[a] else 0u
+    let add a n (M s) = M (s.Add(a, n))
+    let addSingle a (M s) = if contains a (M s) then add a (s.[a]+1u) (M s) else add a 1u (M s)
+    let remove a n (M s) = if s.[a] > n then add a (s.[a]-n) (M s) else M (s.Remove a)
+    let removeSingle a (M s) = if contains a (M s) then remove a 1u (M s) else M (s)
+    let fold f acc (M s) = Map.fold f acc s
+    let foldBack f (M s) acc = Map.foldBack f s acc
