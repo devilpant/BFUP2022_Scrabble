93461035f5451d3d4751fe6e7b1f1c340defb9d2
Peter <pebn@itu.dk>
2022-05-04 10:08:21 +0200

    Insert almost finished

--- ScrabbleBot/Dict.fs
+++ ScrabbleBot/Dict.fs
@@ -12,11 +12,17 @@ open System.Collections.Generic
         | Leaf _ when s.Length = 0 -> Leaf true
         | Node (_, d) when s.Length = 0 -> Node (true, d)
         | Leaf b when s.Length > 0 ->
-          let newDictionary : Dictionary<char, Dict> = Dictionary<char, Dict>() 
-          Node(b, newDictionary.Add(s.[0], insert s.tail empty) )
-        
-        | Node (b, d) when 
+          let newDictionary : Dictionary<char, Dict> = Dictionary<char, Dict>()
+          let substring = s.[1..]
+          newDictionary.Add(s.[0], insert substring (empty()))
+          Node(b, newDictionary)
+        | Node (b, d) when s.Length > 0 -> if d.TryGetValue(s.[0]) then insert substring d(s.[0]) else
+          let newDictionary : Dictionary<char, Dict> = Dictionary<char, Dict>()
+          let substring = s.[1..]
+          d.Add(s.[0], insert substring (empty()))
+          Node(b, newDictionary)
     
+            
     //let lookup (s:string) (D x) = List.exists ((=)s) x
     //let step (c: char) (D dict) = function
          
